d938e4bf24cba5dde1dd8b9b330efb32751a8bc3
Angi Benton <angi.benton@jhu.edu>
2020-11-23 17:06:40 -0500

    still having issues w QP

--- qpsolvers_test.py
+++ qpsolvers_test.py
@@ -1,10 +1,11 @@
+#trying to do this testcase https://link.springer.com/chapter/10.1007%2F3540530320_2
+
 import qpsolvers as qp
 import numpy as np
 
-#trying to do this testcase https://link.springer.com/chapter/10.1007%2F3540530320_2
-
 P = (-100) * np.identity(5).astype(float)
 print("P: " , P.shape)
+print(P.dtype)
 q = np.array([42, 44, 45, 47, 47.5]).astype(float)
 print("q: " , q.shape)
 G = np.array([[20, 12, 11, 7, 4]]).astype(float)
@@ -16,8 +17,6 @@ print("lb: " , lb.shape)
 ub = np.ones((5,)).astype(float)
 print("ub: " , ub.shape)
 
-#WHY DOESNT THIS WORK!!!!!!!!!!!!!!! ohmygod
-
 x_star = qp.solve_qp(P = P, q = q, G = G, h = h, lb = lb, ub = ub)
-
+#waiting on a response from the qpsolvers github people on why this produces an error - if its obvious to anyone please lmk!
 print(x_star)
